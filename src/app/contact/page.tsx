20f7f9cdaa15226272d91707ee99740a7e17659e
chihabMe <chihab.mg.me@gmail.com>
2025-07-23 15:19:00 +0100

    improved the padding in the contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,7 +4,7 @@ import { Footer } from "@/components/Footer";
 
 export default function ContactPage() {
   return (
-    <div className="min-h-screen container max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen container max-w-screen-2xl mx-auto px-4 sm:px-1 md:px-4 lg:px-8">
       <Header />
       <main>
         <ContactSection />
